af122f9f222f4106d1e9eaf1b90dbf092d1ca00a
milliorn <scottmilliorn@gmai.com>
2023-06-13 01:46:15 -0700

    moved props

--- src/components/Swipe.tsx
+++ src/components/Swipe.tsx
@@ -1,18 +1,13 @@
-import { ReactNode } from "react";
 
 import { Container } from "../styles/StyledTitle";
 import Description from "./Description";
+import { SwipeContainerProps } from "../props/SwipeContainerProps";
 import Tags from "./Tags";
 import Title from "./Title";
 
 Swipe.Title = Title;
 Swipe.Tags = Tags;
 Swipe.Description = Description;
-
-interface SwipeContainerProps {
-  image: string;
-  children: ReactNode;
-}
 
 export function Swipe( props: SwipeContainerProps ) {
   return (
