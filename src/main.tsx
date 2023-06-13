381433e88ef0c44021862d103b5f33eca019a39f
milliorn <scottmilliorn@gmai.com>
2023-06-13 01:01:33 -0700

    index

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+
 import 'semantic-ui-css/semantic.min.css'
+
 import "swiper/css"
 import "swiper/css/navigation"
-import App from './App.tsx'
-import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+import App from './App.tsx'
+
+ReactDOM.createRoot( document.getElementById( 'root' ) as HTMLElement ).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
